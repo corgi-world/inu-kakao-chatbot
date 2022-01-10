94d7f1a54f478a54bd96ec6ee9c06d8ee2cb74c3
corgi-world <developer.msw@gmail.com>
2022-01-11 01:05:36 +0900

    update 간소화

--- covid/index.js
+++ covid/index.js
@@ -1,7 +1,7 @@
 import { call } from "./api.js";
 import { xmlToObj, objToTxt } from "./parser.js";
 
-const callApi = async (offset) => {
+export const updateCovid = async (offset) => {
   // offset이 -1이면 어제~오늘 데이터, offset이 -2면 그제~어제 데이터
 
   // 총 이틀치의 데이터가 필요한 이유는 일일 확진자 증가량을 계산하기 때문에
@@ -14,14 +14,9 @@ const callApi = async (offset) => {
     return null;
   }
   const sidoObj = xmlToObj.sidoInfState(sidoXml);
+  if (sidoObj === null) {
+    return null;
+  }
 
   return objToTxt.sidoInfState(sidoObj);
-};
-
-export const updateCovid = async (offset) => {
-  const result = await callApi(offset);
-  if (result !== null) {
-    return result;
-  }
-  return null;
 };
